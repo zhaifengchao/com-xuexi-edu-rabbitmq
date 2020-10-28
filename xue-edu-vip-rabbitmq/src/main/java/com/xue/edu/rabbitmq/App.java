c60db2560d0b23482a930b46b09f8c8cf448e53a
翟风朝 <zhaifengchao1@163.com>
2020-10-28 08:22:06 +0800

    初版

---
+++ xue-edu-vip-rabbitmq/src/main/java/com/xue/edu/rabbitmq/App.java
@@ -0,0 +1,13 @@
+package com.xue.edu.rabbitmq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
